6cc90cd3b8c56244da373d2b9ce7d710a817fa0c
Will Wang <WilWang@ashleyfurniture.com>
2022-04-08 18:26:24 +0800

    update

--- spider/admin.py
+++ spider/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Article,Category
 # Register your models here.
+
+class CategoryInLine(admin.StackedInline):
+    model = Category
+    extra = 1
+
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ['author', 'title', 'content']
+    inlines = [CategoryInLine]
+
+admin.site.register(Article, ArticleAdmin)
--- spider/models.py
+++ spider/models.py
@@ -1,18 +1,30 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
-class Category(models.Model):
-    title = models.CharField(max_length=50)
-    def __str__(self):
-        return self.title
+class BaseModel(models.Model):
+    create_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
 
 class Article(models.Model):
     id = models.BigAutoField(primary_key=True)
     author = models.CharField(max_length=50)
-    create_time = models.DateTimeField
-    categorys = models.ManyToManyField(Category, through='ArticleCategoryShip')
+    create_time = models.DateTimeField(default=datetime.now)
     content = models.CharField(max_length=10000)
+    title = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.title if self.title else ''
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.name
 
 class ArticleCategoryShip(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
